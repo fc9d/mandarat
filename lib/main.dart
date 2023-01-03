21eece684206bbff9f540c06b8d7399ac4a8e92b
teehee <ice1250@gmail.com>
2023-01-03 14:46:14 +0900

    Row 가변사이즈 적용 및 logger 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,181 @@
+import 'dart:developer';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  logger.d('안녕 플러터');
+  runApp(const MyApp());
+}
 
-class MyApp extends StatefulWidget {
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+var loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class Home extends StatefulWidget {
+
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late double fixedSize;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Mandarat'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellowAccent,
-                    width: 200,
-                    height: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('플러터 공부하기',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+    if (MediaQuery.of(context).size.width >
+        MediaQuery.of(context).size.height) {
+
+      fixedSize = MediaQuery.of(context).size.height / 3 - 10;
+    } else {
+      fixedSize = MediaQuery.of(context).size.width / 3 - 10;
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    height: 200,
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('컨플루언스&지라 도입',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.amberAccent,
-                    width: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    height: 200,
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('Github Projects 공부',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.deepOrange,
-                    width: 200,
-                    height: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('아이디어 구상',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.red,
-                    width: 200,
-                    height: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('공부 로드맵',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: const EdgeInsets.all(10.0),
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('서비스명 정하기',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.deepOrangeAccent,
-                    width: 200,
-                    height: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('파이어베이스 연동',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.orange,
-                    width: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    height: 200,
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('애드몹 연동',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                  Container(
-                    color: Colors.orangeAccent,
-                    width: 200,
-                    padding: const EdgeInsets.all(10.0),
-                    height: 200,
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Center(
-                      child: AutoSizeText('VS CODE 알아보기',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
-                    ),
+                ),
+                Container(
+                  width: fixedSize,
+                  height: fixedSize,
+                  color: Colors.yellowAccent,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Center(
+                    child: AutoSizeText('플러터 공부하기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
