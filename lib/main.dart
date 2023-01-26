13188fd0c700fbc352891b870a29c46a301667be
Taehee Ryu <ice1250@gmail.com>
2023-01-26 10:53:19 +0900

    AdMob 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:logger/logger.dart';
 import 'package:mandarat/screens/mandarat_screen.dart';
 import 'package:mandarat/screens/map_screen.dart';
@@ -8,6 +9,7 @@ import 'package:mandarat/widgets/bottom_bar.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
@@ -40,10 +42,50 @@ class _HomeState extends State<Home> {
   List<Widget> views = [MandaratView(), MapScreen()];
 
   int selectIndex = 0;
+
+  late BannerAd myBanner;
+
   void onClicked(int index) {
     setState(() {
       selectIndex = index;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Load ads.
+    // 배너 샘플
+    // ca-app-pub-3940256099942544/6300978111
+
+    MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
+        testDeviceIds: [
+          '6F509068462AD4F9ADFE464ACF4EFFD9',
+          '여기에 너 디바이스 아이디 넣어'
+        ]));
+    myBanner = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        // Called when an ad is successfully received.
+        onAdLoaded: (Ad ad) => print('Ad loaded.'),
+        // Called when an ad request failed.
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          // Dispose the ad here to free resources.
+          ad.dispose();
+          print('Ad failed to load: $error');
+        },
+        // Called when an ad opens an overlay that covers the screen.
+        onAdOpened: (Ad ad) => print('Ad opened.'),
+        // Called when an ad removes an overlay that covers the screen.
+        onAdClosed: (Ad ad) => print('Ad closed.'),
+        // Called when an impression occurs on the ad.
+        onAdImpression: (Ad ad) => print('Ad impression.'),
+      ),
+    );
+
+    myBanner.load();
   }
 
   @override
@@ -56,7 +98,16 @@ class _HomeState extends State<Home> {
         selectedIndex: selectIndex,
         onClicked: onClicked,
       ),
-      body: views[selectIndex],
+      // body: views[selectIndex],
+      body: Column(
+        children: [
+          views[selectIndex],
+          Container(
+            height: 100,
+            child: AdWidget(ad: myBanner),
+          ),
+        ],
+      ),
     );
   }
 }
